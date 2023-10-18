dcccdcc7476bcb41348e6622d59d1a8e0a8deae0
Samrat Baral <71290426+samratbaral@users.noreply.github.com>
2023-10-17 23:28:28 -0500

    Setup

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,27 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // // https://vitejs.dev/config/
-export default defineConfig({ base: "/3d-portfolio/", plugins: [react()] });
+export default defineConfig({
+    base: "/3d-portfolio/",
+    plugins: [react()],
+    build: {
+        chunkSizeWarningLimit: 1600,
+        rollupOptions: {
+            output:{
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                }
+            }
+        }
+    }
+});
 
 // export default defineConfig(({ command }) => {
 //   const config = {
 //     plugins: [react()],
-//     base: "/",
+//     base: "/3d-portfolio/",
 //   };
 
 //   if (command !== "serve") {
