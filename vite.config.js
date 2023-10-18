760160db3564481baf7a1f5cc276d58ef88d2df9
Samrat Baral <71290426+samratbaral@users.noreply.github.com>
2023-10-17 21:27:15 -0500

    deployment stuff

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "https://github.com/samratbaral/3d-portfolio/",
-  plugins: [react()],
-});
+// // https://vitejs.dev/config/
+// export default defineConfig({base: "/3d-portfolio/",plugins: [react()],});
+
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/3d-portfolio/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/react-vite-gh-pages/'
+  }
+
+  return config
+})
