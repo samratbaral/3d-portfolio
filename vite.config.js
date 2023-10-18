c8f4f103594118a22be37b9c6c3eda92bc0931a4
Samrat Baral <71290426+samratbaral@users.noreply.github.com>
2023-10-17 22:18:03 -0500

    new push

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // // https://vitejs.dev/config/
-export default defineConfig({base: "/",plugins: [react()],});
+export default defineConfig({ base: "/3d-portfolio/", plugins: [react()] });
 
 // export default defineConfig(({ command }) => {
 //   const config = {
